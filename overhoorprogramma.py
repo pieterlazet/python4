3ab9d1820381c6c88feafb0975105ddc0b7c131e
Pieter Lazet <pieter.lazet@coderclass.nl>
2019-09-17 15:40:44 +0200

    a balhaar

---
+++ overhoorprogramma.py
@@ -0,0 +1,12 @@
+def main():
+    main_print_opties()
+
+def main_print_opties():
+    print("="*30)
+    print("|" + " {:>14}" .format("Q = niks")+" "*13+"|")
+    print("|" + " {:>14}" .format("Q = niks")+" "*13+"|")
+    print("|" + " {:>14}" .format("Q = niks")+" "*13+"|")
+    print("="*30)
+
+if __name__ == "__main__":
+    main()
